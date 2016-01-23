5dfcefb2beea79ae4f873d0d4c3e7735b09d2967
Andrew Van Tassel <andrew@andrewvantassel.com>
2016-01-23 13:11:24 -0700

    Added highlight on search

--- client/js/filters.js
+++ client/js/filters.js
@@ -37,4 +37,11 @@ firewaterApp.filter('parseState', function() {
           return 'bell';
         }
     }
+}).filter('highlight', function($sce) {
+    return function(text, phrase) {
+      if (phrase) text = text.replace(new RegExp('('+phrase+')', 'gi'),
+        '<span class="highlighted">$1</span>')
+
+      return $sce.trustAsHtml(text)
+    }
 });
