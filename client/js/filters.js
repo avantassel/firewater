6e69adcc1d3577350a04766f422703707a3e11f2
Andrew Van Tassel <andrew@andrewvantassel.com>
2016-01-23 13:36:50 -0700

    Added inc directive

--- client/js/filters.js
+++ client/js/filters.js
@@ -44,4 +44,70 @@ firewaterApp.filter('parseState', function() {
 
       return $sce.trustAsHtml(text)
     }
+}).directive('incCounter', function(){
+  // https://github.com/Kop4lyf/angular-animation-counter
+	//a function to convert the value to locale
+	function generateLocale(value, locale){
+
+		if(locale && Number.toLocaleString){
+			return (+value).toLocaleString(locale);
+		} else {
+			return value;
+		}
+	}
+
+	return {
+		restrict : 'A',
+		scope : {
+			value : "@",
+			interval : "@",
+			locale : "@"
+		},
+		link: function($scope, element, attrs){
+
+			element.html($scope.value);
+			$scope.$watch('value', function(newVal, oldVal) {
+
+        newVal = (newVal || 0);
+				oldVal = (oldVal || 0);
+
+        newVal = newVal.replace(/,/g,'');
+        oldVal = oldVal.replace(/,/g,'');
+
+				if(+newVal !== +oldVal){
+					setCounter(parseInt(oldVal,10), parseInt(newVal,10));
+				}
+			});
+
+			//function to run counter from start to end
+			function setCounter(start, end){
+				if(start == end){
+					return;
+				}
+				//get steps for interval and the divisions so that we can complete the animations in that interval
+				var steps = 20; //get steps or defaults to 20
+				var divisions = (($scope.interval || 1)*1000)/steps; //divide the number to get divisions for change interval
+				var locale = $scope.locale;
+
+				//SETTING the values of counter to run between difference
+				var diff = end - start, counter = start, incr = (end-start>0);
+				var interval = $scope.interval;//diff/divisions;
+
+				//this is so that form minor differences, the counter should not run unnecessarily
+				// interval = Math.abs(interval)>1 ? interval : (interval>0 ? 1 : -1);
+
+				var intFunc = setInterval(function(){
+
+					//when the counter goes beyond range, clearing the interval
+					if((incr && counter >= end) || (!incr && counter <= end)){
+						element.html(generateLocale(end, locale));
+						clearInterval(intFunc);
+						return;
+					}
+					element.html(generateLocale(Math.round(+counter), locale)); //showing the round off value in the interim
+					counter = ((+counter) + (+interval)); //adding + before variable converts it to number
+				}, steps);
+			}
+		}
+	};
 });
